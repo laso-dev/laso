016453c9276bf3d4881944807f4456017cd01c60
Ruan Martinelli <martinelliruan@gmail.com>
2025-10-10 22:09:34 -0300

    wip

--- packages/ui/app/index.tsx
+++ packages/ui/app/index.tsx
@@ -1,17 +1,11 @@
-import { Link, Route, Switch } from 'wouter'
+import { Route, Switch } from 'wouter'
 import { Home } from './routes/home'
 
-export default function App() {
+export function App() {
   return (
     <>
-      <Link href="/users/1">Profile</Link>
-
-      <Route path="/about">About Us</Route>
-
       <Switch>
-        <Route path="/inbox" component={Home} />
-        <Route path="/users/:name">{(params) => <>Hello, {params.name}!</>}</Route>
-        <Route>404: No such page!</Route>
+        <Route path="/" component={Home} />
       </Switch>
     </>
   )
--- packages/ui/app/main.tsx
+++ packages/ui/app/main.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import App from './'
+import { App } from './'
 
 createRoot(document.getElementById('root')!).render(<App />)
