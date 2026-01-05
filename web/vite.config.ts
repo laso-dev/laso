11d4299e8629aa3f270de7205b80575b96139909
Ruan Martinelli <martinelliruan@gmail.com>
2026-01-05 09:38:00 -0300

    chakra

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 5173,
     proxy: {
